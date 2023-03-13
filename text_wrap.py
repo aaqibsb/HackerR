c0b3882148d14f5cc42ab28f7d8df1749c55f3c3
Aaqib Barodawala <83783527+aaqibsb@users.noreply.github.com>
2023-03-13 04:19:54 -0400

    Add files via upload

---
+++ text_wrap.py
@@ -0,0 +1,10 @@
+import textwrap as tr
+
+def wrap(string, max_width):
+    string=tr.fill(string,max_width)
+    return string
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
